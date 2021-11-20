c127db31e2d2df560060fcdf514ec22861d248c6
Lautaro Gallo <lautarovg147@gmail.com>
2021-11-20 11:09:10 -0300

    correciones de errores pequeños

--- helpers/authHelpers.php
+++ helpers/authHelpers.php
@@ -11,6 +11,7 @@ class AuthHelper {
     public function login($user) {
         $_SESSION['USER_ID'] = $user->id;
         $_SESSION['USER_EMAIL'] = $user->email;
+        
     }
 
     public function checkLoggedIn() {
